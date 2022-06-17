f923ae5ea122f195d4f482f6bb22986417130c0d
JeongHa Lee <l03jeongha@gmail.com>
2022-06-17 18:16:50 +0900

    add '--zipf' argument

--- 2popularity.py
+++ 2popularity.py
@@ -6,6 +6,8 @@ parser.add_argument("--input", "-i", metavar='I', type=str, nargs='?', default='
                     help='input file')
 parser.add_argument("--output", "-o", metavar='O', type=str, nargs='?', default='output.txt',
                     help='output file')
+parser.add_argument("--zipf", "-z", action='store_true',
+                    help='calculate zipf parameter')
 args = parser.parse_args()
 
 
@@ -41,6 +43,8 @@ def save_csv(df, filename, index=0):
 * y axis : % of reference count
 """
 
+"""memdf2.1"""
+
 memdf2 = pd.read_csv(args.input, sep=',', header=0, index_col=0, on_bad_lines='skip')
 memdf2_rw = pd.read_csv(args.input[:-4]+'_rw.csv', sep=',', header=0, index_col=0, on_bad_lines='skip')
 
@@ -72,13 +76,48 @@ memdf2_rw['rw_rank_pcnt'] = memdf2_rw['rwpcnt'].rank(ascending=False, pct=True)
 save_csv(memdf2, args.output, 0)
 save_csv(memdf2_rw, args.output[:-4]+'_rw.csv', 0)
 
+"""zipf"""
+
+from scipy.optimize import minimize
+
+if args.zipf:
+  def loglik(b, x, freqs):  
+    # Power law function
+    Probabilities = x**(-b)
+
+    # Normalized
+    Probabilities = Probabilities/Probabilities.sum()
+
+    # Log Likelihoood
+    Lvector = np.log(Probabilities)
+
+    # Multiply the vector by frequencies
+    Lvector = np.log(Probabilities) * freqs
+
+    # LL is the sum
+    L = Lvector.sum()
+
+    # We want to maximize LogLikelihood or minimize (-1)*LogLikelihood
+    return(-L)
+
+  def zipf_param(freqs):
+    freqs = freqs[freqs != 0]
+    y = freqs.sort_values(ascending=False).to_numpy()
+
+    x = np.array(range(1,len(y)+1))
+
+    s_best = minimize(loglik, [2], args=(x, y))
+    return s_best
+
+
 """memdf2.1 graph"""
 
 #memdf2 = pd.read_csv(args.output, sep=',', header=0, index_col=0, on_bad_lines='skip')
 #memdf2_rw = pd.read_csv(args.output[:-4]+'_rw.csv', sep=',', header=0, index_col=0, on_bad_lines='skip')
 
 """
-#plt.figure(figsize=(12,10))
+plt.figure(figsize=(12,10))
+plt.rcParams.update({'font.size': 17})
 
 #read
 x1 = memdf2['read_rank']
@@ -91,19 +130,28 @@ x3 = memdf2_rw['rw_rank']
 y3 = memdf2_rw['count']
 
 #scatter
-plt.scatter(x1, y1, color='blue', label='read', s=5)
-plt.scatter(x2, y2, color='red', label='write', s=5)
-#plt.scatter(x3, y3, color='green', label='read+write', s=5)
+plt.scatter(x1, y1, color='blue', label='read', s=7)
+plt.scatter(x2, y2, color='red', label='write', s=7)
+plt.scatter(x3, y3, color='green', label='read+write', s=7)
 plt.xscale('log')
 plt.yscale('log')
 plt.ylim([0.5,1e5])
+
+if args.zipf:
+  s_best1 = zipf_param(x1, y1)
+  s_best2 = zipf_param(x2, y2)
+  s_best3 = zipf_param(x3, y3)
+  plt.plot(x1, (2*y1.max())*x1**-s_best1.x, color="skyblue", lw=2, label = "fitted MLE"+str(-s_best1.x))
+  plt.plot(x2, (2*y2.max())*x2**-s_best2.x, color="salmon", lw=2, label = "fitted MLE"+str(-s_best2.x))
+  plt.plot(x3, (2*y3.max())*x3**-s_best3.x, color="limegreen", lw=2, label = "fitted MLE"+str(-s_best3.x))
 
 # legend
 plt.xlabel('ranking')
 plt.ylabel('memory block access count')
 plt.legend(loc='upper right', ncol=1)
 
-plt.show()
+# plt.show()
+plt.savefig(args.output[:-4]+'.png', dpi=300)
 """
 
 fig, ax = plt.subplots(2, figsize=(7,6), constrained_layout=True, sharex=True, sharey=True) # sharex=True: share x axis
@@ -126,8 +174,12 @@ y3 = memdf2_rw['count']
 # read/write graph
 ax[0].scatter(x1, y1, color='blue', label='read', s=5)
 ax[0].scatter(x2, y2, color='red', label='write', s=5)
-#ax[0].set_xscale('log')
-#ax[0].set_yscale('log')
+
+if args.zipf:
+  s_best1 = zipf_param(y1)
+  s_best2 = zipf_param(y2)
+  ax[0].plot(x1, (2*y1.max())*x1**-s_best1.x, color="skyblue", lw=2, label = "fitted MLE"+str(-s_best1.x))
+  ax[0].plot(x2, (2*y2.max())*x2**-s_best2.x, color="salmon", lw=2, label = "fitted MLE"+str(-s_best2.x))
 
 # legend
 ax[0].set_xlabel('ranking')
@@ -139,6 +191,11 @@ ax[1].scatter(x3, y3, color='green', label='read&write', s=5)
 ax[1].set_ylim([0.5,1e5])
 ax[1].set_xscale('log')
 ax[1].set_yscale('log')
+
+if args.zipf:
+  s_best3 = zipf_param(y3)
+  ax[1].plot(x3, (2*y3.max())*x3**-s_best3.x, color="limegreen", lw=2, label = "fitted MLE"+str(-s_best3.x))
+
 # legend
 ax[1].set_xlabel('ranking')
 ax[1].set_ylabel('memory block access count')
