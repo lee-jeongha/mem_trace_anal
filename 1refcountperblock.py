5d9f6881aea2564cc489c3844d46436d9954679a
JeongHa Lee <l03jeongha@gmail.com>
2022-07-18 18:17:10 +0900

    plot graph for counts of instruction type

--- 1refcountperblock.py
+++ 1refcountperblock.py
@@ -14,16 +14,20 @@ import math
 * y axis : memory block access count
 """
 def ref_cnt(inputdf, concat=False):
-    if (concat) :
+    if (concat):
         df = inputdf.groupby(by=['blockaddress', 'type'], as_index=False).sum()
         #print(df)
-    else :
+        return df
+
+    else:
+        type_cnt = inputdf['type'].value_counts()
+
         inputdf = inputdf.replace('readi', 'read')
         inputdf = inputdf.replace('readd', 'read')
 
         df = inputdf.groupby(['blockaddress', 'type'])['blockaddress'].count().reset_index(name='count') # Count rows based on 'blockaddress' & 'type', And name as 'count'
 
-    return df
+        return df, type_cnt['readi'], type_cnt['readd'], type_cnt['write']
 
 def ref_cnt_per_block(input_filename, chunks=100):
     ## 1. use list of chunk
@@ -39,34 +43,42 @@ def ref_cnt_per_block(input_filename, chunks=100):
     """
 
     ## 2. load separate .csv file
-    memdf1 = pd.DataFrame()
+    memdf = pd.DataFrame()
+    readi_cnt = 0; readd_cnt = 0; write_cnt = 0
+
     for i in range(chunks): 
         filename = input_filename+'_'+str(i)+'.csv'
+
         try:
             df = pd.read_csv(filename, sep=',', header=0, index_col=0, on_bad_lines='skip')
-            memdf1_chunk = ref_cnt(df, concat=False)
-            memdf1 = pd.concat([memdf1, memdf1_chunk])
+            memdf_chunk, readi_cnt_chunk, readd_cnt_chunk, write_cnt_chunk = ref_cnt(df, concat=False)
+            memdf = pd.concat([memdf, memdf_chunk])
+
+            readi_cnt += readi_cnt_chunk;   readd_cnt += readd_cnt_chunk;   write_cnt += write_cnt_chunk
+
         except FileNotFoundError:
             print("No file named: ", filename)
             break
 
-    memdf1 = ref_cnt(memdf1, concat=True)
+    memdf = ref_cnt(memdf, concat=True)
 
     # both read and write
-    memdf1_rw = memdf1.groupby(by=['blockaddress'], as_index=False).sum()
-    memdf1_rw['type'] = 'read&write'
+    memdf_rw = memdf.groupby(by=['blockaddress'], as_index=False).sum()
+    memdf_rw['type'] = 'read&write'
 
-    memdf1 = pd.concat([memdf1, memdf1_rw], sort=True)
+    memdf = pd.concat([memdf, memdf_rw], sort=True)
 
-    return memdf1
+    memdf['readi'] = pd.Series([readi_cnt])
+    memdf['readd'] = pd.Series([readd_cnt])
+    memdf['write'] = pd.Series([write_cnt])
 
-"""**memdf1 graph**
-> Specify the axis range (manual margin adjustment required)
-"""
+    return memdf
+
+"""**memdf1 graph**"""
 def digit_length(n):
     return int(math.log10(n)) + 1 if n else 0
 
-def hist_label(subplot, counts, bars, round_range=0):
+def hist_label(subplot, counts, bars, round_range=0, rotation=90):
     if round_range:
         counts = np.round(counts, round_range)
     else:
@@ -75,7 +87,30 @@ def hist_label(subplot, counts, bars, round_range=0):
         height = rect.get_height()
         subplot.text(rect.get_x() + rect.get_width()/3.25, 0.4*height,
                 counts[idx], fontsize=15,
-                ha='center', va='bottom', rotation=90)
+                ha='center', va='bottom', rotation=rotation)
+
+"""memdf1.0 graph"""
+def instruction_cnt_graph(title, filename, readi_cnt, readd_cnt, write_cnt):
+    x = range(3)
+    x_label = ['readi', 'readd', 'write']
+    values = [readi_cnt, readd_cnt, write_cnt]
+    colors = ['dodgerblue', 'c', 'red']
+    handles = [plt.Rectangle((0,0),1,1, color=colors[i]) for i in range(3)]
+
+    cnt_sum = readi_cnt + readd_cnt + write_cnt
+    percentile_values = np.divide(values, cnt_sum/100).round(3)
+
+    fig, ax = plot_frame(1, 1, title=title, xlabel='instruction type', ylabel='instruction count', share_yaxis='col')
+    
+    rects = plt.bar(x, values, color=colors)
+    plt.xticks(x, x_label)
+    plt.yticks([])
+    plt.bar_label(rects, fontsize=20, fmt='%.0f')
+
+    plt.legend(handles, [str(i)+'%' for i in percentile_values], fontsize=15)
+
+    #plt.show()
+    plt.savefig(filename+'_instruction.png', dpi=300)
 
 """memdf1.1 graph"""
 def ref_cnt_graph(df, title, filename):
@@ -156,6 +191,8 @@ if __name__ == "__main__":
 
     memdf1 = ref_cnt_per_block(input_filename=args.input, chunks=args.chunk_group)
     save_csv(memdf1, args.output+'.csv', 0)
+
+    instruction_cnt_graph(title=args.title, filename=args.output, readi_cnt=memdf1.iloc[0, 3], readd_cnt=memdf1.iloc[0, 4], write_cnt=memdf1.iloc[0, 5])
 
     #memdf1 = pd.read_csv(args.output+'.csv', sep=',', header=0, index_col=0, on_bad_lines='skip')
     ref_cnt_graph(memdf1, title=args.title, filename=args.output)
--- plot_graph.py
+++ plot_graph.py
@@ -1,23 +1,15 @@
 import matplotlib.pyplot as plt
 
-def plot_frame(subplot_rows=2, subplot_cols=1, title='', xlabel='xlabel', ylabel='ylabel', log_scale=False, share_xaxis:str or bool=True, share_yaxis:str or bool=True):
-    font_size = 25
-    lable_size = 22
+def plot_frame(subplot_rows=2, subplot_cols=1, title='', xlabel='', ylabel='', log_scale=False, share_xaxis:str or bool=True, share_yaxis:str or bool=True):
+    font_size = 20
+    plt.rc('font', size=font_size)
 
     if subplot_rows == 1 and subplot_cols == 1 :
         fig, ax = plt.subplots(subplot_rows, subplot_cols, figsize=(7, 6), constrained_layout=True)
-        plt.xticks(fontsize=font_size)
-        plt.yticks(fontsize=font_size)
+
     else :
         fig, ax = plt.subplots(subplot_rows, subplot_cols, figsize=(7*subplot_cols, 6*subplot_rows), constrained_layout=True, sharex=share_xaxis, sharey=share_yaxis)
         #plt.ticklabel_format(axis='x', scilimits=(4,4))
-
-        for i in range(subplot_rows):
-            if subplot_cols > 1:
-                for j in range(subplot_cols):
-                    ax[i][j].tick_params(labelsize=lable_size)
-            else:
-                ax[i].tick_params(labelsize=lable_size)
 
     if title != '':
         plt.suptitle(title, fontsize = font_size + 10)
@@ -27,7 +19,9 @@ def plot_frame(subplot_rows=2, subplot_cols=1, title='', xlabel='xlabel', ylabel
         plt.xscale('log')
         plt.yscale('log')
 
-    fig.supxlabel(xlabel, fontsize=font_size)
-    fig.supylabel(ylabel, fontsize=font_size)
+    if xlabel != '':
+        fig.supxlabel(xlabel, fontsize = font_size + 5)
+    if ylabel != '':
+        fig.supylabel(ylabel, fontsize = font_size + 5)
 
     return fig, ax
