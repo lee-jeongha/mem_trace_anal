e830cfdd4e31ebead22226671b44679b61fc3c76
JeongHa Lee <l03jeongha@gmail.com>
2022-07-22 16:42:08 +0900

    rearrange codes

--- corr.py
+++ corr.py
@@ -1,98 +1,116 @@
 import argparse
+import os
 import pandas as pd
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-i", type=str, nargs="?")
-args = parser.parse_args()
-
-df = pd.read_csv(args.i, header=0, index_col=0)
-print(df)
-
-col_type = 'type_pcnt_rank' # 'type_rank'
-
-#---
-read = df[['blockaddress',col_type]][(df['type']=='read')]
-read_l = len(read) #// 10
-read = read.sort_values(by=col_type, ascending=True)[:read_l]
-
-write = df[['blockaddress',col_type]][(df['type']=='write')]
-write_l = len(write) #// 10
-write = write.sort_values(by=col_type, ascending=True)[:write_l]
-
-rw = df[['blockaddress',col_type]][(df['type']=='read&write')]
-rw_l = len(rw) #// 10
-rw = rw.sort_values(by=col_type, ascending=True)[:rw_l]
-#---
-
-rw_merge = pd.merge(read, write, left_on='blockaddress', right_on='blockaddress', how='outer', suffixes = ['_read', '_write'])#, lsuffix='_read', rsuffix='_write')
-print(rw_merge)
-
-#---
-only_read = rw_merge[rw_merge[col_type + '_write'].isnull()]
-only_write = rw_merge[rw_merge[col_type + '_read'].isnull()]
-#---
-print("only_read percentage",len(only_read)/len(rw_merge), "only_write percentage",len(only_write)/len(rw_merge))
-rw_merge = rw_merge.dropna()
-print("\nrw_merge\n",rw_merge, "\nonly_read\n",only_read, "\nonly_write\n",only_write)
-
-#---
-"""rw_merge[col_type + '_read'] = rw_merge[col_type + '_read'].fillna(value=1)
-rw_merge[col_type + '_write'] = rw_merge[col_type + '_write'].fillna(value=1)"""
-#print(rw_merge)
-#---
-
-print(rw_merge.corr())
-
-#---
 import matplotlib.pyplot as plt
-
-plt.scatter(rw_merge[col_type + '_read'], rw_merge[col_type + '_write'])
-plt.show()
-
-#---
 import seaborn as sns
 
-sns.jointplot(rw_merge[col_type + '_read'], rw_merge[col_type + '_write'])
-sns.jointplot(rw_merge[col_type + '_read'], rw_merge[col_type + '_write'], kind="hex", color="#4CB391")
-sns.jointplot(rw_merge[col_type + '_read'], rw_merge[col_type + '_write'], kind="hist", color="#4CB391")
-sns.jointplot(rw_merge[col_type + '_read'], rw_merge[col_type + '_write'], kind="kde", color="#4CB391")
-#---
 
-"""
-from scipy.optimize import curve_fit
-import numpy as np
+def read_write_correlation(df, col_type):
+    read = df[['blockaddress',col_type]][(df['type']=='read')]
+    read_l = len(read) #// 10
+    read = read.sort_values(by=col_type, ascending=True)[:read_l]
 
-def func_powerlaw(x, m, c):
-    return x ** m * c
+    write = df[['blockaddress',col_type]][(df['type']=='write')]
+    write_l = len(write) #// 10
+    write = write.sort_values(by=col_type, ascending=True)[:write_l]
 
-def func_log(x,n):
-    return n ** x
+    #---
+    read_write_rank = pd.merge(read, write, left_on='blockaddress', right_on='blockaddress', how='outer', suffixes = ['_read', '_write'])
 
-def zipf_param(freqs):
+    #---
+    only_read = read_write_rank[read_write_rank[col_type + '_write'].isnull()]
+    only_write = read_write_rank[read_write_rank[col_type + '_read'].isnull()]
+    print("only_read percentage",len(only_read)/len(read_write_rank), "only_write percentage",len(only_write)/len(read_write_rank))
+    #---
+    read_write_rank = read_write_rank.dropna()
+    #print("\nread_write_rank\n",read_write_rank, "\nonly_read\n",only_read, "\nonly_write\n",only_write)
 
-    target_func = func_log
+    #---
+    """read_write_rank[col_type + '_read'] = read_write_rank[col_type + '_read'].fillna(value=1)
+    read_write_rank[col_type + '_write'] = read_write_rank[col_type + '_write'].fillna(value=1)"""
+    #print(read_write_rank)
+    #---
 
-    freqs = freqs[freqs != 0]
-    #y = freqs.sort_values(ascending=False).to_numpy()
-    #x = np.array(range(1, len(y) + 1))
-    y = freqs.sort_values(ascending=False).cumsum().to_numpy()
-    x = np.arange(len(y))
-    x = (x / len(y))
+    return read_write_rank
 
-    popt, pcov = curve_fit(target_func, y, x, maxfev=2000)
-    print(popt)
+def read_write_correlation_graph(df, col_type, title, output_filename):
+    font_size = 20
+    plt.rc('font', size=font_size)
 
-    return popt
+    sns.jointplot(data = df, x = col_type + '_read', y = col_type + '_write')
+    plt.savefig(output_filename+'-1-1.png', dpi=300)
 
-param=df['type_pcnt'][(df['type']=='read')].sort_values(ascending=False)
-p = zipf_param(param)
+    sns.jointplot(data = df, x = col_type + '_read', y = col_type + '_write', kind="hex", color="#4CB391")
+    plt.savefig(output_filename+'-1-2.png', dpi=300)
 
-param_x = np.arange(len(param))
-param_x = (param_x / len(param))
+    sns.jointplot(data = df, x = col_type + '_read', y = col_type + '_write', kind="hist", color="#4CB391")
+    plt.savefig(output_filename+'-1-3.png', dpi=300)
 
-plt.scatter(param_x, param.cumsum())
-plt.plot(func_log(param.cumsum(), p), param.cumsum())
-plt.show()
+    sns.jointplot(data = df, x = col_type + '_read', y = col_type + '_write', kind="kde", color="#4CB391")
+    plt.savefig(output_filename+'-1-4.png', dpi=300)
 
-#np.polyfit(np.log(param_x), param.cumsum(), 1)
-"""
+#------------#
+def dense_ref_cnt_graph(df, title, output_filename):
+    df['dense_blockaddress'] = df['blockaddress'].rank(ascending=False, method='dense')
+
+    read = df[['dense_blockaddress', 'count']][(df['type']=='read')]
+    write = df[['dense_blockaddress', 'count']][(df['type']=='write')]
+
+    #---
+    font_size = 22
+    plt.rc('font', size=font_size)
+    plt.suptitle(title, fontsize = font_size + 10)
+
+    sns.kdeplot(read['dense_blockaddress'], weights=read['count'], legend='read', color='blue', bw_adjust=.001, shade=True)
+    sns.kdeplot(write['dense_blockaddress'], weights=write['count'], legend='write', color='red', bw_adjust=.001, shade=True)
+    #sns.kdeplot(rw['dense_blockaddress'], weights=rw['count'], legend='read&write', color='green', bw_adjust=.01, shade=True)
+    plt.savefig(output_filename+'-2-1.png', dpi=300)
+
+    #---
+    x1 = read['dense_blockaddress']
+    y1 = read['count']
+
+    x2 = write['dense_blockaddress']
+    y2 = write['count']
+
+    fig, ax = plt.subplots(2, figsize=(10,8), sharex=True)
+    plt.suptitle(title, fontsize = font_size + 10)
+
+    ax[0].bar(x1, y1, color='blue', edgecolor='blue', label='read')
+    ax[1].bar(x2, y2, color='red', edgecolor='red', label='write')
+
+    if y1.max() > y2.max():
+        y_range = ax[0].get_ylim()
+        ax[1].set_ylim(y_range)
+    else:
+        y_range = ax[1].get_ylim()
+        ax[0].set_ylim(y_range)
+
+    ax[1].invert_yaxis()
+
+    fig.tight_layout()
+    fig.subplots_adjust(hspace=0.0) # wspace=0.0
+    plt.savefig(output_filename+'-2-2.png', dpi=300)
+
+if __name__ == "__main__" :
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cnt_input", "-c", type=str, nargs='?', default='memdf1')
+    parser.add_argument("--rank_input", "-r", type=str, nargs='?', default='memdf2')
+    parser.add_argument("--output", "-o", type=str, nargs='?', default='output')
+    parser.add_argument("--title", "-t", type=str, nargs='?', default='')
+    args = parser.parse_args()
+
+    path = args.output[:args.output.rfind('/')]
+    if not os.path.exists(path):    # FileNotFoundError: [Errno2] No such file or directory: '~'
+        os.makedirs(path)
+
+    df2 = pd.read_csv(args.rank_input+'.csv', header=0, index_col=0)
+    col_type = 'type_pcnt_rank' # 'type_rank'
+    read_write_rank = read_write_correlation(df2, col_type)
+    print(read_write_rank.corr())
+
+    read_write_correlation_graph(read_write_rank, col_type, args.title, args.output)
+
+    plt.clf()
+    df1 = pd.read_csv(args.cnt_input+'.csv', header=0, index_col=0)
+    dense_ref_cnt_graph(df1, args.title, args.output)
